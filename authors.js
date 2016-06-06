8348e75624268d1d80f6d613c00114e706cbfbef
Roma Lapin <roman.lapin@gmail.com>
2016-06-06 12:38:51 +0300

    Author: op

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-    // { username: 'op', start: '6 Jun 2016', first: '', post: false },
+    { username: 'op', start: '6 Jun 2016', first: '739725855505666048', post: false },
     { username: 'be_art', start: '29 May 2016', first: '736988819715678208' },
 ];
