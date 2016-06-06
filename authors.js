7798940f2a88a84c449a391f68c6af4a69b48b48
Roma Lapin <roman.lapin@gmail.com>
2016-06-06 08:39:33 +0300

    Author: be_art - close

--- authors.js
+++ authors.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-    { username: 'be_art', start: '29 May 2016', first: '736988819715678208', post: false },
+    // { username: 'op', start: '6 Jun 2016', first: '', post: false },
+    { username: 'be_art', start: '29 May 2016', first: '736988819715678208' },
 ];
