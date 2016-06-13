1d6fc37828177ee235031f1edc58a5f7aab4cd4a
Roma Lapin <roman.lapin@gmail.com>
2016-06-13 10:43:07 +0300

    Author: AntonKovalevsky

--- authors.js
+++ authors.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-    // { username: 'AntonKovalevsky', start: '13 Jun 2016', first: '', post: false },
+    { username: 'AntonKovalevsky', start: '13 Jun 2016', first: '742257274836619264', post: false },
     { username: 'op', start: '6 Jun 2016', first: '739725855505666048' },
     { username: 'be_art', start: '29 May 2016', first: '736988819715678208' },
 ];
