cb3df4d209fb546e496a1c4bd42a05f10e6fbd80
Roma Lapin <roman.lapin@gmail.com>
2016-06-27 15:29:12 +0300

    Author: Leonid Nikolaev

--- authors.js
+++ authors.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-    // { username: 'Леонид Николаев', start: '27 Jun 2016', first: '', post: false },
+    { username: 'Леонид Николаев', start: '27 Jun 2016', first: '747352870719229953', post: false },
     { username: 'astepanchuk', start: '20 Jun 2016', first: '744795296027148288'},
     { username: 'AntonKovalevsky', start: '13 Jun 2016', first: '742257274836619264' },
     { username: 'op', start: '6 Jun 2016', first: '739725855505666048' },
