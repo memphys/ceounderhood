b9bb7ec5d8360da4738ad0c52d8165c305aac66b
Roma Lapin <roman.lapin@gmail.com>
2016-07-11 00:39:08 +0300

    Author: jbofsky

--- authors.js
+++ authors.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-    // { username: 'jbofsky', start: '11 Jul 2016', first: '', post: false },
+    { username: 'jbofsky', start: '11 Jul 2016', first: '752248529859571713', post: false },
     { username: 'yfilatovy', start: '4 Jul 2016', first: '750061103997128704' },
     { username: 'Леонид Николаев', start: '27 Jun 2016', first: '747352870719229953'},
     { username: 'astepanchuk', start: '20 Jun 2016', first: '744795296027148288'},
