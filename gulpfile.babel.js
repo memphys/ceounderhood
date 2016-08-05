9a52fc6f871d92211a8e4d1ebab8a835ac98030f
Roma Lapin <roman.lapin@gmail.com>
2016-08-05 18:36:06 +0300

    fix rss links; add telegram channel to menu; hide rss and github menu links

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -124,7 +124,7 @@ task('rss', done => {
     feed.item({
       title: author.username,
       description: render(firstTweet(author)),
-      url: `https://jsunderhood.ru/${author.username}/`,
+      url: `https://ceounderhood.ru/${author.username}/`,
       date: firstTweet(author).created_at,
     });
   });
