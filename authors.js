e418532aca3a822b3e9db2e236954a4c7ea0883d
Roma Lapin <roman.lapin@gmail.com>
2016-08-06 10:29:31 +0300

    save dump
    [ci skip]

--- authors.js
+++ authors.js
@@ -10,5 +10,5 @@ export default [
     { username: 'astepanchuk', start: '20 Jun 2016', first: '744795296027148288' },
     { username: 'AntonKovalevsky', start: '13 Jun 2016', first: '742257274836619264' },
     { username: 'op', start: '6 Jun 2016', first: '739725855505666048' },
-    { username: 'be_art', start: '29 May 2016', first: '736988819715678208' },
+    { realname: 'Кирилл Артеменко', company: 'Бумага Медиа', username: 'be_art', start: '29 May 2016', first: '736988819715678208' },
 ];
