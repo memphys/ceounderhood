2b3b3783665bf272d3f32d4414c99ab25e8c57c9
Roma Lapin <roman.lapin@gmail.com>
2016-08-06 12:09:05 +0300

    update site description; add realname and company for authors

--- authors.js
+++ authors.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-    { username: 'vmakeev', start: '1 Aug 2016', first: '760029772688793600', post: false },
-    { username: 'mofas', start: '24 Jul 2016', first: '757500160041689090' },
-    { username: 'noff', start: '17 Jul 2016', first: '754951838189907969' },
-    { username: 'jbofsky', start: '11 Jul 2016', first: '752248529859571713' },
-    { username: 'yfilatovy', start: '4 Jul 2016', first: '750061103997128704' },
-    { username: 'Леонид Николаев', start: '27 Jun 2016', first: '747352870719229953' },
-    { username: 'astepanchuk', start: '20 Jun 2016', first: '744795296027148288' },
-    { username: 'AntonKovalevsky', start: '13 Jun 2016', first: '742257274836619264' },
-    { username: 'op', start: '6 Jun 2016', first: '739725855505666048' },
+    { realname: 'Владимир Макеев', company: 'Surf', username: 'vmakeev', start: '1 Aug 2016', first: '760029772688793600', post: false },
+    { realname: 'Денис Германенко', company: 'Clever Pumpkin', username: 'mofas', start: '24 Jul 2016', first: '757500160041689090' },
+    { realname: 'Михаил Кечинов', company: 'REES46', username: 'noff', start: '17 Jul 2016', first: '754951838189907969' },
+    { realname: 'Яков Берковский', company: 'Vmedia', username: 'jbofsky', start: '11 Jul 2016', first: '752248529859571713' },
+    { realname: 'Юрий Филатов', company: 'Create', username: 'yfilatovy', start: '4 Jul 2016', first: '750061103997128704' },
+    { realname: 'Леонид Николаев', company: 'Breadhead', username: 'Леонид Николаев', start: '27 Jun 2016', first: '747352870719229953' },
+    { realname: 'Андрей Степанчук', company: 'Aidem', username: 'astepanchuk', start: '20 Jun 2016', first: '744795296027148288' },
+    { realname: 'Антон Ковалевский', company: 'Yellow Banana', username: 'AntonKovalevsky', start: '13 Jun 2016', first: '742257274836619264' },
+    { realname: 'Ольга Павлова', company: 'Собака Павлова', username: 'op', start: '6 Jun 2016', first: '739725855505666048' },
     { realname: 'Кирилл Артеменко', company: 'Бумага Медиа', username: 'be_art', start: '29 May 2016', first: '736988819715678208' },
 ];
